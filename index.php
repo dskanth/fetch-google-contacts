add2f64446ac277648c73ba3844a3629c3c9f70b
Shasi kanth <dskanth.99@gmail.com>
2015-03-19 16:58:39 +0530

    Create index.php
    Need to replace the client id with the actual client id of your application.

---
+++ index.php
@@ -0,0 +1,17 @@
+<html>
+    <head>
+        <meta name="robots" content="noindex" />
+        <title>Import Gmail or Google contacts using Google Contacts Data API 3.0 and OAuth 2.0</title>
+    <style type="text/css">
+		a:link {color:Chocolate;text-decoration: none;}
+		a:hover {color:CornflowerBlue;}
+    </style>
+    </head>
+    <body>
+        
+<br/>
+        <div align="center" >
+        	<a  style="font-size:25px;font-weight:bold;" href="https://accounts.google.com/o/oauth2/auth?client_id=xxxxxxxxxxxxx&redirect_uri=http://localhost/google_contacts/oauth.php&scope=https://www.google.com/m8/feeds/&response_type=code">Click here to Import Gmail Contacts</a>
+        </div>
+    </body>
+</html>
